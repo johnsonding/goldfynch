a34e90397adaaeddb8eb755ec1254c22478c9c91
johnsonding <johnsonding@gmail.com>
2016-08-24 03:47:29 -0500

    updated faq page with top nav section and added back to top button

--- js/creative.js
+++ js/creative.js
@@ -63,4 +63,25 @@
 	
 
 
-})(jQuery); // End of use strict
+})(jQuery); 
+
+$(document).ready(function(){
+      $('body').append('<div id="toTop" class="btn btn-warning"><span class="glyphicon glyphicon-chevron-up"></span> Back to Top</div>');
+    	$(window).scroll(function () {
+			if ($(this).scrollTop() != 0) {
+				$('#toTop').fadeIn();
+			} else {
+				$('#toTop').fadeOut();
+			}
+		}); 
+    $('#toTop').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 600);
+        return false;
+    });
+});
+
+
+
+
+// End of use strict
+
